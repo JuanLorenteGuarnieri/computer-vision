17a5abf5522f911ed0e7e2db1125ae112a8a15ef
Juan Lorente Guarnieri <juanlorenteguarnieri@gmail.com>
2024-10-14 15:47:29 +0200

    added script to execute SuperGlue

---
+++ p3/match.bat
@@ -0,0 +1,1 @@
+python ./SuperGluePretrainedNetwork/match_pairs.py --resize 752 --superglue indoor --max_keypoints 2048  --nms_radius 3 --resize_float --input_dir ./ext/  --input_pairs ./ext/euroc_sample_pairs.txt --output_dir ./ext/ --viz
